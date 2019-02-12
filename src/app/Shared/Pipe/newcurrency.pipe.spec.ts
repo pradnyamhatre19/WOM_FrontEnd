072fe5c3363c159dabb57481cc9861fff8ccec70
pradnyamhatre19 <pradnyamhatre89@gmail.com>
2019-02-12 16:46:30 +0530

    no message

--- src/app/Shared/Pipe/newcurrency.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NewcurrencyPipe } from './newcurrency.pipe';
-
-describe('NewcurrencyPipe', () => {
-  it('create an instance', () => {
-    const pipe = new NewcurrencyPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  host : 'http://localhost:3000/',
-  commonapihost: 'https://dad6d64qd6.execute-api.us-east-1.amazonaws.com/dev/',
-  mastersapihost: 'https://7op2npraz4.execute-api.us-east-1.amazonaws.com/dev/',
-  productapihost: 'https://5ouym387z1.execute-api.us-east-1.amazonaws.com/dev/',
-  projectapihost: 'https://n7w7ajamv7.execute-api.us-east-1.amazonaws.com/dev/'
-  
-};
